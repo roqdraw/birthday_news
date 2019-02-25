37b459adefb686a9676eee957a4aaf768eae0f43
Hansol Kim <hansolkim@Hansols-MacBook-Pro.local>
2019-02-26 10:55:34 +1100

    add login page

--- project/main.rb
+++ project/main.rb
@@ -5,18 +5,40 @@ require 'pg'
 require_relative 'db_config'
 require_relative 'models/bookmark'
 require_relative 'models/user'
-
-
 enable :sessions
+
+
+helpers do
+  def current_user
+    User.find_by(id: session[:user_id])
+  end
+  def logged_in?
+    if current_user 
+      true
+    else
+      false
+    end
+  end
+end
 
 get '/' do
   erb :index
 end
 
 
-get '/session/new'
+post '/session' do
+  user = User.find_by(email: params["email"])
+  if user && user.authenticate(params["password"])
+    session[:user_id] = user.id
+    redirect '/'
+  else
+    erb :login
+  end
+end
 
-erb :login
+
+get '/log_in' do
+  erb :login
 end
 
 
